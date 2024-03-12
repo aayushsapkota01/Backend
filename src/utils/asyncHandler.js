43aada246746c5937b06fbb4c3f6380f88ec70f7
aayushsapkota01 <sapkotaaayush10@gmail.com>
2024-03-11 22:37:53 -0400

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// Using Promise
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// //  async handler using try catch
+
+// // High Order Function used in line below
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next)
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
